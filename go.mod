80dc9574af4cbaff17b0ee9ade6143b65184ebcb
mmd <abusemmd@gmail.com>
2024-05-08 19:26:34 +0330

    redis added

--- go.mod
+++ go.mod
@@ -3,13 +3,23 @@ module mmd/v2mngo
 go 1.21.8
 
 require (
+	google.golang.org/grpc v1.62.1
+	v2ray.com/core v4.19.1+incompatible
+)
+
+require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/redis/go-redis/v9 v9.5.1 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240123012728-ef4313101c80 // indirect
-	google.golang.org/grpc v1.62.1 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
-	v2ray.com/core v4.19.1+incompatible // indirect
+	gorm.io/driver/sqlite v1.5.5 // indirect
+	gorm.io/gorm v1.25.10 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -10,11 +11,27 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/redis/go-redis/v9"
 	"google.golang.org/grpc"
 	"v2ray.com/core/common/uuid"
 )
 
-var Users = make(map[int]string)
+var RDB *redis.Client
+var ctx = context.Background()
+
+func ByteCountSI(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(b)/float64(div), "kMGTPE"[exp])
+}
 
 func procIncome(update tg.Update, tk string, cc *grpc.ClientConn) {
 	bt := tg.Bot{
@@ -24,23 +41,21 @@ func procIncome(update tg.Update, tk string, cc *grpc.ClientConn) {
 	switch update.Message.Text {
 	case "/start":
 		{
-
-			user_uuid, ok := Users[update.Message.From.Id]
-
-			if ok {
-				used := v2rpc.GetUserStat(strconv.Itoa(update.Message.From.Id), cc)
-				bt.SendMessage(fmt.Sprintf("uuid: %s \n\n transfered: %d", user_uuid, used), update.Message.From.Id)
-
-			} else {
+			userUUid, err := RDB.Get(ctx, strconv.Itoa(update.Message.From.Id)).Result()
+			if err != nil {
 				new_uuid := uuid.New()
-				Users[update.Message.From.Id] = new_uuid.String()
 				_, err := v2rpc.Adduser(new_uuid.String(), strconv.Itoa(update.Message.From.Id), cc)
 				if err != nil {
 					bt.SendMessage("failed", update.Message.From.Id)
 					log.Print("err: ", err)
 					return
 				}
-				bt.SendMessage(fmt.Sprintf("consider join @naharlo - /start \n\nhttps://ar3642.top/stat.html?uuid=%s", new_uuid.String()), update.Message.From.Id)
+				RDB.Set(ctx, strconv.Itoa(update.Message.From.Id), new_uuid.String(), 0)
+				bt.SendMessage(fmt.Sprintf("@naharlo - /start \n\nhttps://ar3642.top/stat.html?uuid=%s", new_uuid.String()), update.Message.From.Id)
+
+			} else {
+				used := v2rpc.GetUserStat(strconv.Itoa(update.Message.From.Id), cc)
+				bt.SendMessage(fmt.Sprintf("uuid: %s \n\ntransfered: %s", userUUid, ByteCountSI(int64(used))), update.Message.From.Id)
 
 			}
 
@@ -50,13 +65,18 @@ func procIncome(update tg.Update, tk string, cc *grpc.ClientConn) {
 }
 
 func main() {
-	hookp := flag.String("tg", "hook", "telegram hook endpoint")
-
+	tgToken := flag.String("tg", "bot123", "telegram token")
 	flag.Parse()
+
+	RDB = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
 
 	cc := v2rpc.GetGrpcConn("127.0.0.1:8080")
 
-	http.HandleFunc(fmt.Sprintf("/v2api/%s", *hookp), func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc(fmt.Sprintf("/v2api/%s", *tgToken), func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case "POST":
 			{
@@ -66,11 +86,11 @@ func main() {
 					fmt.Printf("err: %v\n", err)
 				}
 				w.WriteHeader(http.StatusOK)
-				go procIncome(up, *hookp, cc)
+				go procIncome(up, *tgToken, cc)
 			}
 		default:
 			{
-				w.Write([]byte(fmt.Sprintf("%v", Users)))
+				w.Write([]byte("okeymokey"))
 			}
 		}
 
